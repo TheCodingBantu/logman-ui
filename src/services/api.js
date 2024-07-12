04867f56571dd57bf7d1d40ba1a1319468ce972d
pngugi <pngugi@mail.com>
2024-07-12 16:25:06 +0300

    jwt

--- src/services/api.js
+++ src/services/api.js
@@ -13,6 +13,18 @@ function getTokenFromLocalStorage() {
     return user ? user.access : null;
   }
 
+  function getRefreshTokenFromLocalStorage() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return user ? user.refresh : null;
+  }
+
+  function setTokensInLocalStorage(tokens) {
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    user.access = tokens.access;
+    user.refresh = tokens.refresh;
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
 apiClient.interceptors.request.use(
     async config => {
       const token = getTokenFromLocalStorage(); // Implement your function to get token from local storage
@@ -26,6 +38,40 @@ apiClient.interceptors.request.use(
     }
   );
 
+  apiClient.interceptors.response.use(
+    response => {
+      return response;
+    },
+    async error => {
+      const originalRequest = error.config;
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        const refreshToken = getRefreshTokenFromLocalStorage();
+        if (refreshToken) {
+          try {
+            const response = await axios.post(`${import.meta.env.VITE_API_ENDPOINT}accounts/token/refresh/`, {
+              refresh: refreshToken,
+            });
+            const newTokens = response.data;
+
+            console.log(newTokens.refresh)
+            setTokensInLocalStorage(newTokens);
+            apiClient.defaults.headers.common['Authorization'] = `Bearer ${newTokens.access}`;
+            originalRequest.headers['Authorization'] = `Bearer ${newTokens.access}`;
+            return apiClient(originalRequest);
+          } catch (err) {
+            // Handle refresh token failure (e.g., redirect to login)
+            console.error('Refresh token failed', err);
+            // Optionally, clear tokens from localStorage
+            localStorage.removeItem('user');
+            return Promise.reject(err);
+          }
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+  
 
 
 
